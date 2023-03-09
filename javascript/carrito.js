fe1c9410f8eab17e17a8c01ccf723828bb4ba042
juan5691 <juancampos5691@gmail.com>
2023-03-09 15:26:42 -0300

    Correcciones al trabajo final

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -1,232 +1,235 @@
 /* ALUMNO JUAN CAMPOS CURSO JAVASCRIPT COMISION 45360, 
 <-------ENTREGA final------>
+EL PROYECTO SE TRATA DE UN "FLY SHOP ONLINE", VENTA DE EQUIPOS DE PESCA CON MOSCA */
 
-/* EL PROYECTO SE TRATA DE UN "FLY SHOP ONLINE", VENTA DE EQUIPOS DE PESCA CON MOSCA */
-
-// ARRAY DE OBJETOS ARTICULO
-let productosCarrito = [];
+let productosCarrito =  [];
 
 function agregarCarrito(e) {
-  //console.log ("Se hizo click", e.target);
-
   let hijo = e.target;
   let padre = hijo.parentNode;
   let abuelo = padre.parentNode;
-  //console.log(padre);
-
+ 
   let nombre = padre.querySelector("h5").textContent;
   let precio = Number(padre.querySelector("span").textContent);
+  let unitario = Number(padre.querySelector("span").textContent);
   let imagen = abuelo.querySelector("img").src;
 
   let articulo = {
     nombre: nombre,
+    precioUnitario: unitario ,
     precio: precio,
     imagen: imagen,
     cantidad: 1,
   };
 
-  //HICE ESTA FUNCION Y EL CONDICIONAL PARA QUE NO SE REPTAN LOS ARTICULOS PERO 🙁
-  //A PESAR DE QUE FUNCIONA NO SE ACTUALIZA CORECTAMENTE EL RENDER
-  // VOY A TENER QUE SEGUIR TRABAJANDO CON ESTA PARTE DEL CODIGO
-
-  //SE BUSCA PRODUCTO EN EL ARRAY PARA VER SI ESTÁ DUPLICADO
-  /* function ubicarRepetido(repetido){
-  return repetido.nombre == articulo.nombre;
+//👉 DESCOMENTAR ESTA PARTE Y COMENTAR EL PUSH AL CARRITO PARA VER QUE PASA
+// PRIMERO SE BUSCA PRODUCTO EN EL ARRAY PARA VER SI ESTÁ DUPLICADO DESPUES SE PUSHEA 
+//FUNCIONA OK 
+/* function ubicarRepetido(repetido){
+return repetido.nombre == articulo.nombre;
 }
 let resultadoRepetido = productosCarrito.find(ubicarRepetido);
 let ubicado = productosCarrito.indexOf(resultadoRepetido)
-
 if(resultadoRepetido){
-   
-  let cantidad = (productosCarrito[ubicado].cantidad);
-   productosCarrito[ubicado].cantidad = cantidad + 1
-   console.log(productosCarrito[ubicado].cantidad);
+  productosCarrito[ubicado].cantidad = productosCarrito[ubicado].cantidad + 1;
+  productosCarrito[ubicado].precio = productosCarrito[ubicado].cantidad * productosCarrito[ubicado].precioUnitario; 
+  let limpiar = document.getElementById("filaCarrito");
+  limpiar.remove();
 
-let precio =  (productosCarrito[ubicado].precio);
-productosCarrito[ubicado].precio = productosCarrito[ubicado].cantidad * precio;
-
-console.log(productosCarrito[ubicado].precio);
-console.log(productosCarrito);
-  
-}else{
-  productosCarrito.push(articulo);
+ }else{
+  productosCarrito.push(articulo);  
 } */
-  // Y FINALMENTE LO DEJE ASI👇
+ 
+productosCarrito.push(articulo);
 
-  //SE PUSHEA AL ARRAY
-  productosCarrito.push(articulo);
 
-  // SE GUARDA EN LOCALSTORAGE
 
+// SE GUARDA EN LOCALSTORAGE
+// SE GUARDA PERFECTO FUNCIONA OK
   let articuloJson = JSON.stringify(productosCarrito);
   localStorage.setItem("carrito", articuloJson);
 
-  // SE RECUPERA CARRITO DE LOCALSTORAGE Y SE PARSEA EL ARCHIVO JSON A ARRAY
-
+// SE RECUPERA CARRITO DE LOCALSTORAGE Y SE PARSEA EL ARCHIVO JSON A ARRAY
+// SE RECUPERA PERFECTO FUNCIONA OK
   let recuperarCarrito = localStorage.getItem("carrito");
   recuperarCarrito = JSON.parse(recuperarCarrito);
 
-  //console.log(recuperarCarrito);
-
-  //IMAGEN ICONO CARRITO Y CONTADOR
+  
+//IMAGEN ICONO CARRITO Y CONTADOR
+//CUENTA CORRECTAMENTE FUCIONA OK
   let cantidad = recuperarCarrito.length;
-  //console.log(cantidad);
   let verCarrito = document.getElementById("verCarrito");
   verCarrito.innerText = cantidad + "🛒";
   verCarrito.style.color = "rgb(39, 128, 28)";
 
-  if (verCarrito != 0) {
-    verCarrito.style.display = "block";
-  } else {
-    verCarrito.style.display = "none";
-  }
 
   //FUNCION PARA SUMAR TOTAL DEL CARRITO
-
+  //SUMA CORRECTO LO QUE ESTÁ EN LOCALSTORAGE
   function total(acu, producto) {
     acu = acu + producto.precio;
     return acu;
   }
   let totalCarrito = recuperarCarrito.reduce(total, 0);
-
   let sumaTotal = document.getElementById("total");
   sumaTotal.innerText = "$" + totalCarrito;
+ 
 
-  //console.log(totalCarrito);
-
-  //SE CREA UNA FILA VACIA PARA QUE CUANDO SE RECORRA EL ARRAY NO DUPLIQUE EL RENDER
+  // SE VUELVE A TRAER EL CARRITO DEL LOCAL STORAGE Y SE MUESTRAN DESDE AHÍ 
+  // SE RECORRE EL ARRAY Y SE RENDERIZAN LOS OBJETOS RECUPERADOS 
+  let traerCarrito = localStorage.getItem("carrito");
+  traerCarrito = JSON.parse(traerCarrito);
   let fila = document.createElement("tr");
-  fila.innerHTML = "";
-
-  // SE RECORRE EL ARRAY Y SE RENDERIZAN LOS OBJETOS RECUPERADOS EN LA FILA QUE ESTA VIA
-
-  for (let carrito of recuperarCarrito) {
-    fila.innerHTML = ` <td><img src="${carrito.imagen}" class="img-thumbnail" width ="80" height ="80"></td> 
-                       <td><span>${carrito.nombre}</span></td>
-                        <td>${carrito.cantidad}</td>
-                        <td>$${carrito.precio}</td>
-                        <td><button type="button" class="btn btn-outline-success eliminar">Elimnar</button></td>
-                        
+  fila.setAttribute("id", "filaCarrito");
+  
+  for (let carrito of traerCarrito) {
+ 
+   fila.innerHTML = `<td class="carro"><img src="${carrito.imagen}" class="img-thumbnail" width ="80" height ="80"></td> 
+                     <td class="carro"><span>${carrito.nombre}</span></td>
+                     <td class="carro">${carrito.cantidad}</td>
+                     <td class="carro">$${carrito.precio}</td>
+                     <td class="carro"><button type="button" class="btn btn-outline-success eliminar">Elimnar</button></td>
     `;
-
+    // 👉DEJE LOS CONSOLE.LOG PORQUE A VECES EL RENDER NO MUESTRA LO QUE VIENE DEL LOCAL STORAGE
+    // 👉NO ENTIENDO CUAL ES EL PROBLEMA, ES LA MISMA ITERACION, EL CONSOLE.LOG MUESTRA 
+    // LO QUE HAY EN LOCALSTORAGE CORRECTAMENTE PERO EN EL RENDER A VECES SE VE OTRA COSA
+    console.log("Equipo N°" + carrito.nombre);
+    console.log("Cantidad " + carrito.cantidad);
+    console.log("Total $"+carrito.precio);
+    console.log("Total compra "+ totalCarrito);
+    console.log("<------>");
+   
     // SE CAPTURA EL NODO Y SE INSERTA EL CONTENIDO
-
     let tabla = document.getElementById("tbody");
     tabla.append(fila);
-
     // SE CAPTURA BOTON ELIMINAR SE GENERA EL EVENTO
-
     let botonEliminar = document.querySelectorAll(".eliminar");
     for (let boton of botonEliminar) {
-      boton.addEventListener("click", eliminarArticulo);
+    boton.addEventListener("click", eliminarArticulo);
     }
   }
 }
 
-// BOTON ELIMINAR
 
+
+// BOTON ELIMINAR
 function eliminarArticulo(e) {
   let equipo = e.target.parentNode.parentNode;
-
   //SE IDENTIFICA EL NOMBRE DEL EQUIPO QUE SE ELIMINO DEL CARRITO
   nombreEquipo = equipo.querySelector("span").textContent;
-  //console.log(nombreEquipo);
 
-  // SE RECUPERA CARRITO DE LOCALSTORAGE SE PARSEA Y SE HACE UN FIND() CON EL NOMBRE DEL EQUIPO IDENTIFICADO
+// SE RECUPERA CARRITO DE LOCALSTORAGE SE PARSEA Y SE HACE UN FIND() CON EL NOMBRE DEL EQUIPO IDENTIFICADO
   let recuperarCarrito = localStorage.getItem("carrito");
   recuperarCarrito = JSON.parse(recuperarCarrito);
-  //console.log(recuperarCarrito);
-
   function buscarEquipo(equipo) {
     return equipo.nombre == nombreEquipo;
   }
 
   let equipoEncontrado = recuperarCarrito.find(buscarEquipo);
   let indice = recuperarCarrito.indexOf(equipoEncontrado);
-  //console.log(indice);
   recuperarCarrito.splice(indice, 1);
-  //console.log(recuperarCarrito);
+
   //SE SOBREESCRIBE LOCALSTORAGE, ---NO PUDE ELIMINAR CON localStorage.removeItem()--
   let nuevoCarrito = JSON.stringify(recuperarCarrito);
   localStorage.setItem("carrito", nuevoCarrito);
 
   //VUELVO A HACER LO MISMO PARA EL ARRAY productos[] PARA QUE NO EMPIECE DE NUEVO EL CICLO
   //AL VOLVER A CLICKEAR UN PRODUCTO SIN ANTES REFRESCAR LA PAGINA, NO ME LO VUELVA A MANDAR AL LOCALSTORAGE
-
   function ubicarEquipo(equipoUbicado) {
-    return equipoUbicado.nombre == nombreEquipo;
+  return equipoUbicado.nombre == nombreEquipo;
   }
   let equipoUbicacion = productosCarrito.find(ubicarEquipo);
-  //console.log(equipoUbicacion);
   let indiceEquipo = productosCarrito.indexOf(equipoUbicacion);
-  //console.log(indiceEquipo);
   productosCarrito.splice(indiceEquipo, 1);
 
-  //let eliminar = JSON.stringify(equipoEncontrado);
-  //localStorage.removeItem("carrito" , );
-  //FUNCION PARA SUMAR TOTAL DEL CARRITO
-
   //SE VUELVE A CALCULAR EL TOTAL DEL CARRITO SUANDO SE ELIMINAN ARTICULOS
+  //SUMA CORRECTAMENTE AUN CUANDO SE ELIMINAN ARTICULOS
   function total(acu, producto) {
     acu = acu + producto.precio;
     return acu;
   }
   let totalCarrito = recuperarCarrito.reduce(total, 0);
-
   let sumaTotal = document.getElementById("total");
   sumaTotal.innerText = "$" + totalCarrito;
 
-  //IMAGEN ICONO CARRITO Y CONTADOR CUANDO SE ELIMINA DEL CARRITO SE ACTUALIZA CONTADOR
-  let cantidad = recuperarCarrito.length;
-  console.log(cantidad);
-  let verCarrito = document.getElementById("verCarrito");
-  verCarrito.innerText = cantidad + "🛒";
-  verCarrito.style.color = "rgb(39, 128, 28)";
-
-  if (verCarrito != 0) {
-    verCarrito.style.display = "block";
-  } else {
-    verCarrito.style.display = "none";
-  }
-
-  //SE ELIMINA DEL HTML
+ //IMAGEN ICONO CARRITO Y CONTADOR CUANDO SE ELIMINA DEL CARRITO SE ACTUALIZA CONTADOR
+ // FUNCIONA OK CUENTA LA CANTIDAD DE ARTICULOS, NO DE UNIDADES 
+ let cantidad = recuperarCarrito.length;
+ let verCarrito = document.getElementById("verCarrito");
+ verCarrito.innerText =`${cantidad} 🛒`;
+ verCarrito.style.color = "rgb(39, 128, 28)";
+ //SE ELIMINA DEL HTML
   e.target.parentNode.parentNode.remove();
 }
 
-//SE TRAE EL BOTON SE GENERA ENTO CLICK AGREGA AL CARRITO
+
+//SE TRAE EL BOTON SE GENERA EVENTO CLICK AGREGA AL CARRITO
 let botonComprar = document.querySelectorAll(".botonComprar");
 //SE RECORRE EL ARRAY DE BOTONES
 for (let boton of botonComprar) {
-  boton.addEventListener("click", agregarCarrito);
+boton.addEventListener("click", agregarCarrito);
 
-  //MOSTRAR CARRITO
+//MOSTRAR CARRITO
+//FUNCIONA OK SE MUESTRA AL COMPRAR
+boton.addEventListener("click", mostrar);
+function mostrar() {
 
-  boton.addEventListener("click", mostrar);
-  function mostrar() {
-    let productosCarrito = document.getElementById("carrito");
-    productosCarrito.style.display = "block";
+  let productosCarrito = document.getElementById("carrito");
+  productosCarrito.style.display = "block";
   }
 }
 
-//FUNCION PARA MOSTRAR Y OCULTAR CARRITO👇 TAMBIEN SE MUESTRA AL CLICKEAR BOTON COMPRA
-
 //OCULTAR CARRITO
+//FUNCIONA OK
 let botonOcultar = document.getElementById("ocultarCarrito");
 botonOcultar.addEventListener("click", ocultar);
 function ocultar() {
-  // console.log("boton escuchado")
-  let productosCarrito = document.getElementById("carrito");
-  productosCarrito.style.display = "none";
+let productosCarrito = document.getElementById("carrito");
+productosCarrito.style.display = "none";
+let limpiar = document.getElementById("filaCarrito");
+limpiar.remove();
 }
-
 let verCarrito = document.getElementById("verCarrito");
 verCarrito.addEventListener("click", mostrar);
+
+// FUNCION MOSTRAR CLICK EN ICONO CARRITO
 function mostrar() {
-  let productosCarrito = document.getElementById("carrito");
-  productosCarrito.style.display = "block";
+  let traerCarrito = localStorage.getItem("carrito");
+  traerCarrito = JSON.parse(traerCarrito);
+  let fila = document.createElement("tr");
+  fila.setAttribute("id", "filaCarrito");
+  
+  for (let carrito of traerCarrito) {
+ 
+   fila.innerHTML = `<td class="carro"><img src="${carrito.imagen}" class="img-thumbnail" width ="80" height ="80"></td> 
+                     <td class="carro"><span>${carrito.nombre}</span></td>
+                     <td class="carro">${carrito.cantidad}</td>
+                     <td class="carro">$${carrito.precio}</td>
+                     <td class="carro"><button type="button" class="btn btn-outline-success eliminar">Elimnar</button></td>
+    `;
+    // 👉DEJE LOS CONSOLE.LOG PORQUE A VECES EL RENDER NO MUESTRA LO QUE VIENE DEL LOCAL STORAGE
+    // 👉NO ENTIENDO CUAL ES EL PROBLEMA, ES LA MISMA ITERACION, EL CONSOLE.LOG MUESTRA 
+    // LO QUE HAY EN LOCALSTORAGE CORRECTAMENTE PERO EN EL RENDER A VECES SE VE OTRA COSA
+    console.log("Equipo N°" + carrito.nombre);
+    console.log("Cantidad " + carrito.cantidad);
+    console.log("Total $"+carrito.precio);
+    console.log("Total compra "+ totalCarrito);
+    console.log("<------>");
+   
+    // SE CAPTURA EL NODO Y SE INSERTA EL CONTENIDO
+    let tabla = document.getElementById("tbody");
+    tabla.append(fila);
+    // SE CAPTURA BOTON ELIMINAR SE GENERA EL EVENTO
+    let botonEliminar = document.querySelectorAll(".eliminar");
+    for (let boton of botonEliminar) {
+    boton.addEventListener("click", eliminarArticulo);
+    }
+  }
+ /*  let productosCarrito = document.getElementById("carrito");
+  productosCarrito.style.display = "block"; */
 }
 
 //FUNCION ASINCRONICA
+//FUNCIONA OK ENCIENDE INTERMITENTE BOTON CHECKOUT
 function botonCheckout() {
   let botonCheckout = document.getElementById("checkout");
   if (botonCheckout.className == "btn btn-success eliminar") {
@@ -235,18 +238,53 @@ function botonCheckout() {
     botonCheckout.className = "btn btn-success eliminar";
   }
 }
-
 let intervalo = setInterval(botonCheckout, 1500);
 
-function proximamente() {
-  Swal.fire({
-    text: "Proximamente pasarela de pagos ⚡",
-    icon: "success",
-    background: "black",
-    confirmButtonColor: "rgb(16, 56, 40)",
-    color: "white",
-  });
+//FINALIZACION DE LA COMPRA
+function listado(){
+  let traerCarrito = localStorage.getItem("carrito");
+  traerCarrito = JSON.parse(traerCarrito);
+  function totalCarrito(acu, producto) {
+    acu = acu + producto.precio;
+    return acu;
+  }
+  let total = traerCarrito.reduce(totalCarrito, 0);
+  let totalCompra = document.getElementById("totalCompra");
+  totalCompra.innerText= "Total de su compra es de:  $" + total;
+
+  for (let sku of traerCarrito){
+    let listar = document.createElement("ul"); 
+    listar.innerHTML=`  
+    <li>Articulo: ${sku.nombre}</li>
+    <li>Cantidad: ${sku.cantidad}</li>
+    <li>Precio unitario: ${sku.precioUnitario}</li>
+    <br>
+    `
+    let agregarListado = document.getElementById("listado");
+    agregarListado.append(listar);
+  }
+}
+let compra = document.getElementById("checkout");
+compra.addEventListener("click" , listado);
+
+function ocultar() {
+let productosCarrito = document.getElementById("carrito");
+productosCarrito.style.display = "none";
+}
+let desaparecer = document.getElementById("checkout");
+desaparecer.addEventListener("click" , ocultar);
+
+function mostrarFinalizar(){
+let finalizar = document.getElementById("finalizacion");
+finalizar.style.display="block"
 }
 
-let checkout = document.getElementById("checkout");
-checkout.addEventListener("click", proximamente);
+let finalizaCompra = document.getElementById("checkout");
+finalizaCompra.addEventListener("click" , mostrarFinalizar);
+
+function adios(){
+localStorage.removeItem("carrito");
+location.href="index.html";
+}
+let fin = document.getElementById("ok");
+fin.addEventListener("click", adios);
